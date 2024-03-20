8e78d681ed1354a537d4598489d2da0fad1fb3bf
ujjwal-kr <epicujjwal@protonmail.com>
2024-03-20 14:08:01 +0530

    add status to api

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import pyttsx3
 import speech_recognition as sr
 import google.generativeai as genai
 from dotenv import dotenv_values
+from flask import Flask, jsonify
+import threading
 
 config = dotenv_values(".env")
-print(config)
 
 recogniser = sr.Recognizer()
 microphone = sr.Microphone()
@@ -14,34 +15,47 @@ speaker.setProperty('voice', voices[1].id)
 speaker.say("Hello world, I am Vyomee")
 speaker.runAndWait()
 
-config = dotenv_values(".env")
-
-print(config)
-
 r = sr.Recognizer()
 mic  = sr.Microphone()
 genai.configure(api_key=config["GEMINI_KEY"])
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
 
-with open("prompt.txt") as f:
-    prompt = f.read()
+status = ""
+app = Flask(__name__)
 
-chat.send_message(prompt)
+@app.route("/status")
+def get_status():
+    return jsonify({"status": status})
+
+def start_flask_server():
+    app.run()
+
+if __name__ == "__main__":
+    flask_thread = threading.Thread(target=start_flask_server)
+    flask_thread.start()
+
+    with open("prompt.txt") as f:
+        prompt = f.read()
+
+    chat.send_message(prompt)
 
 while True:
     print("Listening...")
+    status = "listening"
     with mic as source:
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
 
-    print("Processing now...")
+    status = "processing"
+    print("Processing now")
     try:
         data = r.recognize_wit(audio, config["WIT_KEY"])
         print("User said:", data)
         response = chat.send_message(data)
         text_response = response.candidates[0].content.parts[0].text
         print(text_response)
+        status = "speaking"
         speaker.say(text_response)
         speaker.runAndWait()
         
@@ -49,5 +63,5 @@ while True:
         speaker.say("Sorry, could not understand. Please repeat.")
         speaker.runAndWait()
     except sr.RequestError as e:
-        speaker.say("Critical error occured")
+        speaker.say("Critical error occurred")
         speaker.runAndWait()
